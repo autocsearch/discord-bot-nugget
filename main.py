f844f34c9bd650037aa52b408be181543a94e5f5
autosearch <105331946+charlescornelio@users.noreply.github.com>
2025-12-18 15:43:48 +0700

    adding days greetings

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ class Client(discord.Client):
         
         if message.content.startswith('hello'):
             await message.channel.send(f'hi there {message.author}')
+
+        if message.content.lower().startswith('morning'):
+            await message.channel.send(f'Good Morning, {message.author}')
+        if message.content.lower().startswith('evening'):
+            await message.channel.send(f'Good evening, {message.author}')
+        if message.content.lower().startswith('afternoon'):
+            await message.channel.send(f'Good afternoon, {message.author}')
+        if message.content.lower().startswith('night'):
+            await message.channel.send(f'Good night, {message.author}')
+
     async def on_reaction_add(self, reaction, user):
         await reaction.message.channel.send('you reacted')
 
