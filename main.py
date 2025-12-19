8acd88208e0b66fce3669fc7f67def864ce15aa0
autosearch <105331946+charlescornelio@users.noreply.github.com>
2025-12-19 17:31:29 +0700

    creating adding button with slash command

--- main.py
+++ main.py
@@ -38,7 +38,13 @@ class Client(commands.Bot):
         if message.content.lower().startswith('night'):
             await message.channel.send(f'Good night, {message.author}')
         if message.content.lower().startswith("lmao"):
-                embed = discord.Embed(title="Man You Suck 👎👎",description="lol")
+                embed = discord.Embed(title= "i am a Title", description="i am a description", url="https://www.youtube.com/watch?v=wUf1Vgp-2CE", color=discord.Color.gold())
+                embed.set_thumbnail(url="https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435339_1280.png")
+                embed.add_field(name="text 1", value="value 1", inline=False)
+                embed.add_field(name="text 2", value="value 2", inline=True)
+                embed.add_field(name="text 3", value="value 3", inline=True)
+                embed.set_footer(text="this is a footer")
+                embed.set_author(name=message.author, url="https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435339_1280.png", icon_url="https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435339_1280.png")
                 await message.channel.send(embed=embed)
             
 
@@ -71,6 +77,22 @@ async def embed(interaction: discord.Interaction):
     await interaction.response.send_message(embed=embed)
 
 
+class View(discord.ui.View):
+    @discord.ui.button(label="Click me button 1!", style=discord.ButtonStyle.red, emoji="🙍‍♂️")
+    async def button_callback(self, button, interaction):
+        await button.response.send_message("You have clicked the button 1")
+
+    @discord.ui.button(label="Click me Button 2!", style=discord.ButtonStyle.green, emoji="🧑")
+    async def two_button_callback(self, button, interaction):
+        await button.response.send_message("You have clicked the button 2")
+
+    @discord.ui.button(label="Click me Button 3!", style=discord.ButtonStyle.grey, emoji="👮‍♂️")
+    async def three_button_callback(self, button, interaction):
+        await button.response.send_message("You have clicked the button 3")
+
+@client.tree.command(name="button", description="Displaying a button", guild=discord.Object(id=int(SERVER)))
+async def myButton(interaction: discord.Interaction):
+    await interaction.response.send_message(view=View())
 
 
 client.run(TOKEN)
